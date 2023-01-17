b6eb554e2e4fa53657c888522479fca01f628ce6
0thw0foxa0 <97839499+0thw0foxa0@users.noreply.github.com>
2023-01-17 12:04:34 +0700

    Add files via upload

---
+++ docker/python-app/Main.py
@@ -0,0 +1,9 @@
+import calendar
+
+print('Добро пожаловать вкал\n')
+
+year = int(input('введи год'))
+month = int(input('введи месяц'))
+print(calendar.month(year,month))
+
+print('всего хорошего')
